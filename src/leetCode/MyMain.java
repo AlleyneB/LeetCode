3e8a0f5af838de2285a7a6aff18a6a6a59df86b3
Alleyne <libo7989@gmail.com>
2018-03-13 22:02:27 +0800

    first solution

---
+++ src/leetCode/MyMain.java
@@ -0,0 +1,11 @@
+package leetCode;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.firstUniqChar("qwertwer"));
+	}
+	
+
+}
