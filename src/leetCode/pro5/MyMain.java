511b35fc39ec2e45d5f5d5b6f3e8673ef540596e
Alleyne <libo7989@gmail.com>
2018-03-29 22:28:24 +0800

    not a efficiently method

---
+++ src/leetCode/pro5/MyMain.java
@@ -0,0 +1,9 @@
+package leetCode.pro5;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.champagneTower(2, 1, 1));
+	}
+}
