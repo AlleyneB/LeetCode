b887e7d542953a164795020d4ec9f12acabc1403
Alleyne <libo7989@gmail.com>
2018-04-07 11:35:28 +0800

    No.738

---
+++ src/leetCode/pro9/MyMain.java
@@ -0,0 +1,11 @@
+package leetCode.pro9;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.monotoneIncreasingDigits(332));
+	}
+	
+
+}
