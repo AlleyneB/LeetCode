633baf157ec5b2ccb309085f0454233ab5be66f3
Alleyne <libo7989@gmail.com>
2018-04-09 20:45:49 +0800

    10,3.无重复最长子串

---
+++ src/leetCode/pro10/MyMain.java
@@ -0,0 +1,11 @@
+package leetCode.pro10;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.lengthOfLongestSubstring("aaabb"));
+	}
+	
+
+}
