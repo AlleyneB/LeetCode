32158b08b6ddf611aad1cc8235254091743bc4a5
Alleyne <libo7989@gmail.com>
2018-05-08 23:16:43 +0800

    最大单词长度乘积

---
+++ src/leetCode/pro015/MyMain.java
@@ -0,0 +1,10 @@
+package leetCode.pro015;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		String[] words = {"a", "aa", "aaa", "aaaa"};
+		System.out.println(solution.maxProduct(words));
+	}
+}
